a8d8c3daabfc546a53e295f1a79c6f042939f5bd
jihoon <jip68660@gmail.com>
2020-01-06 16:55:42 -0600

    Transfer from old repository

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
-
-import ExampleComponent from 'img-iframe-carousel'
+import './App.css'
+import { Carousel } from 'img-iframe-carousel'
+import image1 from './1.jpeg'
+import image2 from './2.jpeg'
+import image3 from './3.jpeg'
+import image4 from './4.jpg'
+import image5 from './5.jpg'
+import image6 from './download.jpeg'
 
 export default class App extends Component {
   render () {
+    const images = [image1, image2, image3, image4, image5, image6]
+
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div className='container'>
+        <Carousel images={ images } />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,70 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import styles from './carousel.css'
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+export class Carousel extends Component {
+  constructor (props) {
+    super(props)
+    this.state=({
+      index: 0
+    })
+    this.maxLength = props.images.length
+    this.slideToLeft = this.slideToLeft.bind(this)
+    this.slideToRight = this.slideToRight.bind(this)
+  }
+  slideToLeft = () => {
+    if (this.state.index === 0) {
+      this.setState({
+        index: this.maxLength - 1
+      })
+    }
+    else {
+      this.setState((prevState)=>({
+        index: prevState.index - 1
+      }))
+    }
+  }
+  slideToRight = () => {
+    if (this.state.index === this.maxLength - 1) {
+      this.setState({
+        index: 0
+      })
+    }
+    else {
+      this.setState((prevState)=>({
+        index: prevState.index + 1
+      }))
+    }
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
+    // const { images } = this.props.images
+    const imagesList = this.props.images.map((image, index) => {
+      if (index == this.state.index) {
+        return(
+          <img 
+            key={ index } 
+            className={ `${styles.slides} ${styles.actived}` } 
+            src={ image }
+          />
+        )
+      }
+      else {
+        return(
+          <img 
+            key={ index } 
+            className={ `${styles.slides} ${styles.blurry}`} 
+            src={ image } 
+          />
+         )
+      }
+    })
+    return(
+      <div className={ styles.carousel_container }>        
+        { imagesList }
+        <div>
+          <button className={ styles.buttons } onClick={ this.slideToLeft }>left</button>
+          <button className={ styles.buttons } onClick={ this.slideToRight }>right</button>
+        </div>
       </div>
     )
   }
